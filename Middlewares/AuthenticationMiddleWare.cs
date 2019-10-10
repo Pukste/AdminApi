23d94c92bebecd178f70356c683f02357cc100cf
Pukste <ville_pukki@hotmail.com>
2019-10-10 06:17:39 +0300

    apikey added

---
+++ Middlewares/AuthenticationMiddleWare.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using UsingOptions.Models;
+ 
+namespace gamewebapi.Middlewares
+{
+    public class AuthenticationMiddleware
+    {
+ 
+        private readonly MyOptions _options;
+        private readonly RequestDelegate    _next;
+ 
+        public AuthenticationMiddleware(RequestDelegate next, IOptions<MyOptions> optionAccessor){
+ 
+            _options = optionAccessor.Value;
+            _next = next;
+        }
+ 
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.ContainsKey("ApiKey"))
+            {
+ 
+                context.Response.StatusCode = 400;
+            }else if (!context.Request.Headers["ApiKey"].Equals(_options.ApiKey))
+            {
+                context.Response.StatusCode = 401;
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+ 
+ 
+       
+    }
+}
